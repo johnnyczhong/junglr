c1ee7627354ab6b94bc1d45a9335e44e646c9e68
Johnny Zhong <johnnyczhong@gmail.com>
2016-08-25 18:22:56 -0700

    the beginning of the end of not-so-random functions. code refactoring ahoy!

--- db_helper.py
+++ db_helper.py
@@ -5,34 +5,33 @@ from pgdb import connect
 from config import aws_db, aws_host, aws_user, aws_pass
 
 # init connect with db
-def Connect():
-    try:
-        connection = connect(database=aws_db, host=aws_host, user=aws_user, password=aws_pass)
-    except:
-        connection = False
-    return connection
+# everything is an object!
+# pipe to db; has methods to read and write.
+class Connection():
+    def __init__(self):
+        try:
+            self.connection = connect(database=aws_db, host=aws_host, user=aws_user, password=aws_pass)
+        except:
+            self.connection = False
 
-#used for create and update queries
-#returns true if successful
-def Commit(query, conn):
-    committed = False    
-    try:
-        with conn.cursor() as c:
+    def Commit(self, query):
+        committed = False    
+        try:
+            with self.connection.cursor() as c:
+                c.execute(query)
+                self.connection.commit()
+                committed = True
+        except:
+            self.connection.rollback()
+        return committed
+
+    def Fetch(self, query, amt = 1):
+        row = None
+        with self.connection.cursor() as c:
             c.execute(query)
-            conn.commit()
-            committed = True
-    except:
-        conn.rollback()
-    return committed
+            if type(amt) == int:
+                row = c.fetchmany(amt)
+            elif amt == 'all':
+                row = c.fetchall()
+        return row
 
-#used for select queries
-#returns row if successful, None if not
-def Fetch(query, conn, amt = 1):
-    row = None
-    with conn.cursor() as c:
-        c.execute(query)
-        if type(amt) == int:
-            row = c.fetchmany(amt)
-        elif amt == 'all':
-            row = c.fetchall()
-    return row
