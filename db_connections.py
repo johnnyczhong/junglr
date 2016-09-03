69192068805e3c61abe067eeea2ddf7999a618dd
Johnny Zhong <johnnyczhong@gmail.com>
2016-09-03 15:24:22 -0700

    rethinking how my logic and object responsibilities should be placed, in light of the prospect of queued multithreading my api calls

--- db_connections.py
+++ db_connections.py
@@ -19,19 +19,8 @@ class Player_Data_CRUD():
 		self.summ_id = summ_id
 		self.table_affected = 'player_data'
 
-	def Get_Summ_ID(self):
-		return self.api_summ_info['id']
-
-	def Get_API_Revision_Date(self):
-		return self.api_summ_info['revisionDate']
-
-    #in: summ info hash
-	#out: summ name
-	def Get_API_Summ_Name(self):
-		return self.api_summ_info['name']
-
 	#read action for summoner data
-	def Read_Summ_Row(self): 
+	def read_summ_row(self): 
 		action = 'select'
 		where_hash = {'summ_id' : self.summ_id}
 		select_query = Query(action, self.table_affected, where = where_hash)
@@ -48,55 +37,28 @@ class Player_Data_CRUD():
 	    where_hash = {'summ_id' : self.summ_id}
 	    delete_query = Query(action, self.table_affected, where = where_hash)
 	    deleted = self.db_conn.Execute_Query(delete_query.statement)
-	    if self.Read_Summ_Row() == None:
+	    if self.read_summ_row() == None:
 	        deleted = True
 	    return deleted
 
-	#updates or creates based on api data vs db data
-    #return: 'update' to update, 'create' to create, 'no action' if no action
-	def Update_Create(self, api_summ_info):
-		action = 'no action'
-		self.api_summ_info = api_summ_info
-		self.api_rev_date = self.Get_API_Revision_Date()
-		self.summ_name = self.Get_API_Summ_Name()
-		self.sql_row = self.Read_Summ_Row()
-
-		if not self.Create_Summoner_Decision():
-			self.Create_Summoner()
-			action = 'create'
-		elif self.Update_Summoner_Decision():
-  			self.Update_Summoner()
-  			action = 'update'
-		return action
-
-	#determines if summoner db record should be created
-	def Create_Summoner_Decision(self):
-		exists = False if self.sql_row == [] else True
-		return exists
-
 	#in: dict of singular summ info
 	#out: true if player record created, false if not
-	def Create_Summoner(self):
+	def create_summoner(self, summ_name, api_rev_date):
 	    action = 'insert'
-	    subject_hash = {'summ_id' : self.summ_id, 'summ_name' : self.summ_name, 'revision_date' : self.api_rev_date}
+	    subject_hash = {'summ_id' : self.summ_id, 'summ_name' : summ_name, 'revision_date' : api_rev_date}
 	    create_query = Query(action, self.table_affected, subject = subject_hash)
 	    created = self.db_conn.Execute_Query(create_query.statement)
 	    return created
 
-	#determines if summoner db should be updated
-	def Update_Summoner_Decision(self):
-		update = True if self.sql_row[0].revision_date < self.api_rev_date else False
-		return update
-
-	def Update_Summoner(self):
+	def update_summoner(self, summ_name, api_rev_date):
 		action = 'update'
-		subject_hash = {'summ_name' : self.summ_name, 'revision_date' : self.api_rev_date}
+		subject_hash = {'summ_name' : summ_name, 'revision_date' : api_rev_date}
 		where_hash = {'summ_id' : self.summ_id}
 		update_query = Query(action, self.table_affected, where = where_hash, subject = subject_hash)
 		updated = self.db_conn.Execute_Query(update_query.statement)
 		return updated
 
-	def Add_Mains(self, main_lane, main_champ, main_role):
+	def add_Mains(self, main_lane, main_champ, main_role):
 		action = 'update'
 		subject_hash = {'main_champ' : main_champ, 'main_lane' : main_lane, 'main_role' : main_role}
 		where_hash = {'summ_id' : self.summ_id}
@@ -104,7 +66,7 @@ class Player_Data_CRUD():
 		added = self.db_conn.Execute_Query(add_query.statement)
 		return added
 
-	def Add_League(self, league, division):
+	def add_League(self, league, division):
 		action = 'update'
 		subject_hash = {'league' : league, 'division' : division}
 		where_hash = {'summ_id' : self.summ_id}
--- player_api.py
+++ player_api.py
@@ -8,65 +8,103 @@ from db_connections import Player_Data_CRUD, Champ_Data_CRUD
 #player object
 #used to map information to database
 #call methods to populate values
+#make decisions regarding pulling and pushing data
+#all api calls need to be 1 call for easier tracking, unless absolutely necessary.
 class Player():
     def __init__(self, summ_name):
         self.summ_name = summ_name.lower()
-        self.summ_id = self.summ_name_to_id()
-        self.summ_api_data = self.get_api_data()
-        self.api_revision_date = self.get_api_revision_date()
+        self.summ_id = -1
+        self.summ_api_data = {}
+        self.api_rev_date = -1
+        self.match_list = []
+        self.main_champ_id = -1
+        self.main_champ_name = ''
+        self.main_lane = ''
+        self.main_role = ''
+        self.player_db_conn = Player_Data_CRUD(self.summ_id)
+        self.player_db_row = () #named tuple
+        self.api_calls = 0
 
-    #given summoner name, determine summoner id
-    def summ_name_to_id(self):
+    def get_summ_id(self):
+        return self.summ_id
+
+    def get_summ_api_data(self):
+        return self.summ_api_data
+
+    def get_api_rev_date(self):
+        return self.api_rev_date
+
+    def pull_player_api_data(self):
         req = api_request('summ_name_to_id', self.summ_name)
         player_info = req.make_request()
         summ_id = player_info[self.summ_name]['id']
-        return summ_id
+        revision_date = player_info[self.summ_name]['revisionDate']
+        self.summ_id = summ_id
+        self.summ_api_data = player_info[str(self.summ_name)]
+        self.api_rev_date = revision_date
+        self.api_calls += 1
 
-    def get_api_data(self):
-        req = api_request('basic_summ_info', self.summ_id)
-        player_info = req.make_request()
-        return player_info[str(self.summ_id)]
+    def pull_match_list(self):
+        opt_params = {
+        'rankedQueues' : 'TEAM_BUILDER_DRAFT_RANKED_5x5', 
+        'seasons' : 'SEASON2016'
+        }
+        self.match_list_data = Player_Matches(self.summ_id, optional_api_params = opt_params)
+        self.api_calls += 1
 
-    #with summoner id retrieved, determine last api revision date (date updated)
-    def get_api_revision_date(self):
-        revision_date = self.summ_api_data['revisionDate']
-        return revision_date
-
-    def get_match_list(self):
-        opt_params = {'rankedQueues' : 'TEAM_BUILDER_DRAFT_RANKED_5x5', 'seasons' : 'SEASON2016'}
-        self.ranked_match_list_data = Player_Matches(self.summ_id, optional_api_params = opt_params)
-
-    def get_ranked_mains(self):
-        self.get_match_list()
-        self.main_champ_id = self.ranked_match_list_data.calc_main_champ()
-        self.main_lane = self.ranked_match_list_data.calc_main_lane()
-        self.main_role = self.ranked_match_list_data.calc_main_role()
+    def set_ranked_mains(self):
+        self.pull_match_list()
+        self.main_champ_id = self.match_list_data.calc_main_champ()
+        self.set_main_champ_name()
+        self.main_lane = self.match_list_data.calc_main_lane()
+        self.main_role = self.match_list_data.calc_main_role()
 
     #should the player object be pushing database updates?
     # i guess? it seems like the main driver for interactions is the player object
     # as it is also the main focus and is used to populate all other databases.
 
-    def get_league(self):
+    def set_league(self):
         req = api_request('league_entry', self.summ_id)
         league_data = (req.make_request())[str(self.summ_id)][0]
         self.league = league_data['tier']
         self.division = league_data['entries'][0]['division']
+        self.api_calls += 1
 
-    def push_update(self):
-        player_db_conn = Player_Data_CRUD(self.summ_id)
-        if (player_db_conn.Update_Create(self.summ_api_data) != 'no action'):
-            self.get_ranked_mains()
-            player_db_conn.Add_Mains(self.main_lane, self.get_champ_name(), self.main_role)
-            self.get_league()
-            player_db_conn.Add_League(self.league, self.division)
+    def pull_update(self):
+        self.set_ranked_mains()
+        self.set_league()
+        
+    def push_db_update(self):
+        self.player_db_conn.add_Mains(self.main_lane, self.main_champ_name, self.main_role)
+        self.player_db_conn.add_League(self.league, self.division)
+
+    def data_is_current(self):
+        action = 'no action'
+        self.player_db_row = self.player_db_conn.read_summ_row()
+
+        if not self.needs_create(): #creates summoner row in player_data table
+            self.player_db_conn.create_summoner(self.summ_name, self.api_rev_date)
+            action = 'create'
+        elif self.needs_update(): #updates summoner row in player_data table
+            self.player_db_conn.update_summoner(self.summ_name, self.api_rev_date)
+            action = 'update'
+        return action
+
+    def needs_create(self):
+        exists = False if self.player_db_row == [] else True
+        return exists
+
+    def needs_update(self):
+        update = True if self.player_db_row[0].revision_date < self.api_rev_date else False
+        return update
 
     #want to put this through a rate limiter queue first
     # def pull_api_data(self, api_req):
     #     return api_req.make_request() #actually gets put into a queue.
 
-    def get_champ_name(self):
+    def set_main_champ_name(self):
         champ_name = Champ_Data_CRUD().champ_id_to_name(self.main_champ_id)
-        return champ_name
+        self.main_champ_name = champ_name
 
 
 class Player_Matches():
@@ -74,7 +112,12 @@ class Player_Matches():
         self.summ_id = summ_id
         #out: list of hash objects with some information provided
         #TODO: begin_time and end_time
-        self.match_list_basics = self.get_match_list(optional_api_params)
+        self.match_list_basics = self.pull_match_list(optional_api_params)
+        self.match_id_list = []
+        self.champ_freq_list = []
+        self.lane_freq_list = []
+        self.role_freq_list = []
+        self.others_ids = []
 
     #game-v1.3 api endpoint
     #pulls the same/similar data as provided by post-match screen
@@ -83,14 +126,13 @@ class Player_Matches():
 
     #gets list of matches as specified by optional params
     #decorator to add specifity to matches retrieved?
-    def get_match_list(self, optional_api_params):
+    def pull_match_list(self, optional_api_params):
         req = api_request('match_list', self.summ_id, optional_params = optional_api_params)
         response = req.make_request()
         return response['matches']
 
     #out: list of match ids
     def get_match_id_list(self):
-        self.match_id_list = []
         for matches in self.match_list_basics:
             self.match_id_list.append(matches['id'])
 
@@ -128,36 +170,25 @@ class Player_Matches():
     #     #TODO: make match data object for each match
 
     #returns hash of summoner information, namely {id: id, name, revisionDate}
-    def allies_and_opponents(self):
-        others_ids_string = ','.join(self.others_ids)
-        req = api_request('basic_summ_info', others_ids_string)
-        others_info = req.make_request()
-        return others_info
+    # TODO:
+    # def allies_and_opponents(self):
+    #     others_ids_string = ','.join(self.others_ids)
+    #     req = api_request('basic_summ_info', others_ids_string)
+    #     others_info = req.make_request()
+    #     return others_info
 
 
 class Match_Details():
     def __init__(self, match_id):
         self.match_id = match_id
         # straight riot api data pull
-        self.details = self.get_match_details()
+        self.details = self.pull_match_details()
 
-    def get_match_details(self):
+    def pull_match_details(self):
         req = api_request('match_details', self.match_id)
         return req.make_request()
 
 
-# player make match list - representing aggregate player information (that can be obtained via match list)
-    # match list object represents aggregate match statistics that can only be obtained via the match details
-    # match list with function calc champ main returns value to player object 
-    # match list make match data objects
-        # match data object has data members for statistics in a single match
-
-# user make request to site, request added to queue
-# 
-# init queue
-# queue is evaluated
-# user inputs summoner name - requests added to queue
-# 
 
 
 
